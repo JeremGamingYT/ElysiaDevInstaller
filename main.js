c6d8f3a05ca83acd75cf6d15274723ff0b1e79b1
JeremGaming <JeremGaming124@gmail.com>
2024-10-15 23:59:54 -0400

    Updating interface & methods

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ let mainWindow
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 600,
-    height: 400,
+    width: 400,
+    height: 600,
     resizable: false,
+    fullscreenable: false,
+    maximizable: false,
     icon: path.join(__dirname, 'assets', 'icon.png'),
     webPreferences: {
       nodeIntegration: true,
@@ -29,7 +31,7 @@ app.on('window-all-closed', () => {
 })
 
 ipcMain.handle('install-forge', async (event) => {
-  const forgeUrl = 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/forge/forge-installer.jar'
+  const forgeUrl = 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/forge/forge-installer.jar'
   const installerPath = path.join(app.getPath('temp'), 'forge-installer.jar')
   try {
     const response = await axios.get(forgeUrl, { responseType: 'stream' })
@@ -47,26 +49,33 @@ ipcMain.handle('install-forge', async (event) => {
       writer.on('finish', resolve)
       writer.on('error', reject)
     })
-    exec(`"${installerPath}"`, (error) => {
-      if (error) event.sender.send('install-forge-reply', { success: false })
-      else event.sender.send('install-forge-reply', { success: true })
-    })
-  } catch {
+    // Exécution de l'installateur Forge
+    exec(`java -jar "${installerPath}"`, { maxBuffer: 1024 * 500 }, (error) => {
+      if (error) {
+        console.error(`Erreur lors de l'installation de Forge: ${error.message}`)
+        event.sender.send('install-forge-reply', { success: false })
+      } else {
+        event.sender.send('install-forge-reply', { success: true })
+      }
+    })    
+  } catch (error) {
+    console.error(`Erreur lors du téléchargement de Forge: ${error.message}`)
     event.sender.send('install-forge-reply', { success: false })
   }
 })
 
 ipcMain.handle('install-mods', async (event) => {
   const mods = [
-    { name: 'AmbientSounds_FORGE_v6.1.1_mc1.20.1.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/AmbientSounds_FORGE_v6.1.1_mc1.20.1.jar' },
-    { name: 'Animation_Overhaul-forge-1.20.x-1.3.1.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/Animation_Overhaul-forge-1.20.x-1.3.1.jar' },
-    { name: 'BetterAnimationsCollection-v8.0.0-1.20.1-Forge.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/BetterAnimationsCollection-v8.0.0-1.20.1-Forge.jar' },
-    { name: 'BetterThirdPerson-Forge-1.20-1.9.0.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/BetterThirdPerson-Forge-1.20-1.9.0.jar' },
-    { name: 'BiomesOPlenty-forge-1.20.1-19.0.0.91.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/BiomesOPlenty-forge-1.20.1-19.0.0.91.jar' },
-    { name: 'BuildPasteMod-1.20.1v1.11.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/BuildPasteMod-1.20.1v1.11.jar' },
-    { name: 'ChatNotify-Forge-1.20.1-1.3.0.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/ChatNotify-Forge-1.20.1-1.3.0.jar' },
-    { name: 'Chipped-forge-1.20.1-3.0.6.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/Chipped-forge-1.20.1-3.0.6.jar' },
-    { name: 'Clumps-forge-1.20.1-12.0.0.4.jar', url: 'https://github.com/JeremGamingYT/ElysiaDevInstaller/blob/main/Installer/mods/Clumps-forge-1.20.1-12.0.0.4.jar' }
+    { name: 'AmbientSounds_FORGE_v6.1.1_mc1.20.1.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/AmbientSounds_FORGE_v6.1.1_mc1.20.1.jar' },
+    { name: 'Animation_Overhaul-forge-1.20.x-1.3.1.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/Animation_Overhaul-forge-1.20.x-1.3.1.jar' },
+    { name: 'BetterAnimationsCollection-v8.0.0-1.20.1-Forge.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/BetterAnimationsCollection-v8.0.0-1.20.1-Forge.jar' },
+    { name: 'BetterThirdPerson-Forge-1.20-1.9.0.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/BetterThirdPerson-Forge-1.20-1.9.0.jar' },
+    { name: 'BiomesOPlenty-forge-1.20.1-19.0.0.91.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/BiomesOPlenty-forge-1.20.1-19.0.0.91.jar' },
+    { name: 'BuildPasteMod-1.20.1v1.11.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/BuildPasteMod-1.20.1v1.11.jar' },
+    { name: 'ChatNotify-Forge-1.20.1-1.3.0.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/ChatNotify-Forge-1.20.1-1.3.0.jar' },
+    { name: 'Chipped-forge-1.20.1-3.0.6.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/Chipped-forge-1.20.1-3.0.6.jar' },
+    { name: 'Clumps-forge-1.20.1-12.0.0.4.jar', url: 'https://raw.githubusercontent.com/JeremGamingYT/ElysiaDevInstaller/main/Installer/mods/Clumps-forge-1.20.1-12.0.0.4.jar' }
+    // Ajoutez les autres mods de la même manière
   ]
   const modsDestPath = path.join(app.getPath('appData'), '.minecraft', 'mods')
 
@@ -102,6 +111,7 @@ ipcMain.handle('install-mods', async (event) => {
     }
     event.sender.send('install-mods-reply', { success: true })
   } catch (error) {
+    console.error(`Erreur lors de l'installation des mods: ${error.message}`)
     event.sender.send('install-mods-reply', { success: false })
   }
 })
--- renderer.js
+++ renderer.js
@@ -1,30 +1,25 @@
 const { ipcRenderer } = require('electron')
+
 const installForgeBtn = document.getElementById('installForge')
 const installModsBtn = document.getElementById('installMods')
 const forgeProgress = document.getElementById('forgeProgress')
 const modsProgress = document.getElementById('modsProgress')
 const statusMessage = document.getElementById('statusMessage')
 
-const bootstrap = require('bootstrap')
-
-Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]')).forEach(tooltipTriggerEl => {
-  new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
 installForgeBtn.addEventListener('click', () => {
-  forgeProgress.classList.remove('d-none')
+  forgeProgress.classList.remove('hidden')
   installForgeBtn.disabled = true
-  statusMessage.textContent = 'Téléchargement et installation de Forge en cours...'
+  statusMessage.textContent = 'Installation de Forge en cours...'
   ipcRenderer.invoke('install-forge')
 })
 
 ipcRenderer.on('forge-download-progress', (event, percentage) => {
-  const progressBar = forgeProgress.querySelector('.progress-bar')
+  const progressBar = forgeProgress.querySelector('div')
   progressBar.style.width = `${percentage}%`
 })
 
 ipcRenderer.on('install-forge-reply', (event, arg) => {
-  forgeProgress.classList.add('d-none')
+  forgeProgress.classList.add('hidden')
   installForgeBtn.disabled = false
   if (arg.success) {
     statusMessage.textContent = 'Forge a été installé avec succès.'
@@ -34,19 +29,19 @@ ipcRenderer.on('install-forge-reply', (event, arg) => {
 })
 
 installModsBtn.addEventListener('click', () => {
-  modsProgress.classList.remove('d-none')
+  modsProgress.classList.remove('hidden')
   installModsBtn.disabled = true
-  statusMessage.textContent = 'Téléchargement et installation des mods en cours...'
+  statusMessage.textContent = 'Installation des mods en cours...'
   ipcRenderer.invoke('install-mods')
 })
 
 ipcRenderer.on('mods-download-progress', (event, data) => {
-  const progressBar = modsProgress.querySelector('.progress-bar')
+  const progressBar = modsProgress.querySelector('div')
   progressBar.style.width = `${data.percentage}%`
 })
 
 ipcRenderer.on('install-mods-reply', (event, arg) => {
-  modsProgress.classList.add('d-none')
+  modsProgress.classList.add('hidden')
   installModsBtn.disabled = false
   if (arg.success) {
     statusMessage.textContent = 'Les mods ont été installés avec succès.'
